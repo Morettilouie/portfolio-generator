fe315bcf3218c23ad408b70fbc30fc50ad261b2b
Louie Moretti <morettilouie@gmail.com>
2022-07-01 22:29:33 -0500

    lesson trouble

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==========');
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
 
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+
+const profileDataArgs = process.argv.slice(2);
+
+
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
